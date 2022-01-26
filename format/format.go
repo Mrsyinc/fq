f653a377edacfab288caffded402a1e6fe4e5615
Mattias Wadman <mattias.wadman@gmail.com>
2022-01-26 16:44:44 +0100

    pdf: Add decoder

--- format/format.go
+++ format/format.go
@@ -83,6 +83,7 @@ const (
 	OPUS_PACKET         = "opus_packet"
 	PCAP                = "pcap"
 	PCAPNG              = "pcapng"
+	PDF                 = "pdf"
 	PNG                 = "png"
 	PROTOBUF            = "protobuf"
 	PROTOBUF_WIDEVINE   = "protobuf_widevine"
